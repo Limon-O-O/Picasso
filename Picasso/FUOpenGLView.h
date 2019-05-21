042dfcd4750cf6ae3a015e403149ffe3a45c1801
Limon <fengninglong@gmail.com>
2019-05-21 20:56:03 +0800

    add OpenGL view

---
+++ Picasso/FUOpenGLView.h
@@ -0,0 +1,17 @@
+//
+//  FUOpenGLView.h
+//  Picasso
+//
+//  Created by Limon F. on 11/4/2019.
+//  Copyright © 2019 Picasso. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface FUOpenGLView : UIView
+
+- (void)displayPixelBuffer:(CVPixelBufferRef)pixelBuffer;
+
+- (void)displayPixelBuffer:(CVPixelBufferRef)pixelBuffer withLandmarks:(float *)landmarks count:(int)count;
+
+@end
---
+++ Picasso/OpenGLView.swift
@@ -0,0 +1,27 @@
+//
+//  OpenGLView.swift
+//  Picasso
+//
+//  Created by Limon F. on 11/4/2019.
+//  Copyright © 2019 Picasso. All rights reserved.
+//
+
+import UIKit
+
+class OpenGLView: Renderable {
+
+    var view: UIView
+
+    init() {
+        view = FUOpenGLView(frame: UIScreen.main.bounds)
+        view.contentMode = UIView.ContentMode.scaleAspectFit
+    }
+
+    func renderImage(_ image: CIImage) {
+
+    }
+
+    func renderPixelBuffer(_ pixelBuffer: CVPixelBuffer) {
+        (self.view as? FUOpenGLView)?.display(pixelBuffer)
+    }
+}
